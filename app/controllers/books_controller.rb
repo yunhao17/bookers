5c3d92444d06319b3e7414334ab7e9d6617a2b9a
yunhao17 <yunhao17@gmail.com>
2021-05-13 08:32:50 +0000

    [Start]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,48 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  
+  def create
+    @books =  Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:success] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:success] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else 
+      render :edit
+    end
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    if book.destroy
+      flash[:success] = "Book was successfully destroyed."
+    redirect_to books_path
+    end
+  end  
+  private
+  def book_params
+    params.require(:book).permit(:title,  :body)
+  end
+end
